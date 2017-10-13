4c00ec723d6d89f67fd0039a17e0cecbc8031446
choyai <choyaichaiyo@gmail.com>
2017-10-13 21:38:35 +0700

    Linked

--- FIBO_Portfolio/activities/urls.py
+++ FIBO_Portfolio/activities/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    #/activities/
+    url(r'^$', views.index, name='index'),
+    #/activities/create
+    url(r'^create$', views.createActivity, name='createActivity')
 ]
--- FIBO_Portfolio/activities/views.py
+++ FIBO_Portfolio/activities/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    return render(request, 'activities/index.html')
+
+def createActivity(request):
+    return render(request, 'activities/createActivity.html')
